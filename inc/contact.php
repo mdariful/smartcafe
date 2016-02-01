1d672d33288bb77e3bf93d2d657a5ff569b813b5
Md Ariful <md.ariful@email.com>
2016-02-01 13:40:40 +0100

    Update contact.php

--- inc/contact.php
+++ inc/contact.php
@@ -1,13 +1,17 @@
 <?php
-//if "email" variable is filled out, send email
-  if (isset($_REQUEST['email']))  {
-  
   //Email information
   $to_email = "info@smartcafe.it";
   $email = $_REQUEST['email'];
   $subject = "E-mail inviato utilizzando il modulo di contatto";
   $message = $_REQUEST['message'];
-  
+ 
+function is_valid_email($email) {
+  return preg_match('#^[a-z0-9.!\#$%&\'*+-/=?^_`{|}~]+@([0-9.]+|([^\s]+\.+[a-z]{2,6}))$#si', $email);
+}
+
+if (!is_valid_email($email)) {
+  exit;
+}
   //send email
-  mail($to_email, "$subject", $message, "From:" . $email);}
+  mail($to_email, "$subject", $message, "From:" . $email);
 ?>
